83711cee865dfb070c283c5e740e011d5fdea988
BelemsigriWindyamJusteYannel <windyamjusteyannel.belemsigri@usmba.ac.ma>
2025-02-04 23:04:46 +0100

    with interface

--- New Struct/src/model/gui.py
+++ New Struct/src/model/gui.py
@@ -1,46 +1,90 @@
+"""
 import streamlit as st
-from sentence_transformers import SentenceTransformer
-
+from sentence_transformers import SentenceTransformer, util
 
 
 # Charger le modèle d'embeddings
 model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
-# Ajouter le chemin du dossier parent au sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from model import semantique_search
-from model import send_question_lmstudio_LLM
+# Liste de documents
+documents = [
+    "Les examens doivent se dérouler dans le respect des horaires définis.",
+    "Les étudiants doivent présenter leur carte d'étudiant lors des évaluations.",
+    "Tout retard au début des épreuves sera sanctionné par une exclusion. L'usage des téléphones",
+    "L'usage des téléphones portables pendant les évaluations est strictement interdit.",
+    "Une note inférieure à 5/20 est considérée comme un échec à l'évaluation."
+]
 
 # Fonction pour charger le CSS
-def load_css(file):
-    with open(file) as f:
+def load_css(file_name):
+    with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Charger le CSS
-css_file ="./../css/style.css"
-load_css(css_file)
+load_css("style.css")
 
-# Fonction principale
-def __main__() :
+# Titre de l'application
+st.title("📚Règlement Intérieur des Évaluations à l'EST")
+st.markdown("---")
+
+# Champ de saisie de la requête utilisateur
+query = st.text_input("🔍 **Entrez votre requête de recherche :**")
+
+# Si une requête est saisie
+if query:
+    st.markdown("### Résultat de la recherche:")
+    # Calculer l'embedding de la requête
+    query_embedding = model.encode(query)
+
+   
+    doc_embeddings = model.encode(documents)
+
+     # Calculer la similarité entre la requête et les documents
+    scores = util.cos_sim(query_embedding, doc_embeddings)[0]
+
+    #recuperer l'id de la similarité le plus  élévé en terme de score .
+    best_idx = scores.argmax()
+
+    best_score = scores[best_idx].item()
+    best_doc = documents[best_idx] #Recuperer la meilleur reponse à travers son id . 
+
+    # Afficher le résultat
+    st.success(f"{best_doc}")
+    #st.info(f"Confiance : {best_score:.4f}")
+else:
+    st.info("Veuillez entrer une requête dans le champ ci-dessus.")
+
+"""
+
+
+from model import get_model_response
+import streamlit as st
+
+st.title("Renseignement")
+
+# Where we will store questions history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Now were will display our history in the interface
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# User input
+if prompt:=st.chat_input("what's up?"):
+    # Were're going to display user message in the interface
+    with st.chat_message("user"):
+        st.markdown(prompt)
     
-    st.title("📚Règlement Intérieur des Évaluations à l'EST")
-    st.markdown("---")
+    # We will add the user message to the history
+    st.session_state.messages.append({"role":"user","content":prompt})
 
-    # Champ de saisie de la requête utilisateur
-    query = st.text_input("🔍 **Entrez votre requête de recherche :**")
-
-    # Si une requête est saisie
-    if query:
-        st.markdown("### Résultat de la recherche:")
-
-        # Generer l'embedding de la requête
-        results = semantique_search(query)
-
-        
-        # Obtenir une réponse en utilisant LMStudio
-        reponse = send_question_lmstudio_LLM(query, results)
-        
-        #st.success(f"{results}")
-        st.success(f"{reponse}")
-    else:
-        st.info("Veuillez entrer une requête dans le champ ci-dessus.")
+    response = f"Echo : {get_model_response(prompt)}"
+    # We are now about to display the response
+    with st.chat_message("assistart"):
+        st.markdown(response)
+    
+    # Finally we will add to the history the response
+    st.session_state.messages.append({"role":"assistant","content":response})
